1c5e10c46a5c97045586589f009c5b25d4c8a6f4
preps47 <marco@premoli.net>
2025-09-22 06:38:18 +0200

    test

--- src/image.rs
+++ src/image.rs
@@ -158,9 +158,7 @@ impl BMPImage {
         I: IntoIterator<Item = (usize, u32)>
     {
         for x in 0..self.width as usize {
-            println!("{}", x);
             for (y, color) in f(x) {
-                println!("{}, {}", y, color);
                 if y < self.height as usize {
                     self.set_pixel(x, y, color);
                 }
